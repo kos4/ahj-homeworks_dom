54677a47d3ce2faa55d89f91d7e8847088dfff1d
kos4 <kos4.kks@gmail.com>
2024-06-12 19:36:35 +0500

    fix

--- src/js/__tests__/GamePlay.js
+++
@@ -1,9 +0,0 @@
-import GamePlay from "../classes/GamePlay";
-
-test('GamePlay', () => {
-  const game = new GamePlay();
-  const num = game.getRandomNumber(0,4,1);
-  let result = num >= 0 && num < 4 && num !== 1;
-  expect(result).toBe(true);
-})
-
